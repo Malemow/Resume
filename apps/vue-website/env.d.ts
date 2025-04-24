f5319fb186667405dc60970700ba6d0d53d65cf9
Nick Lin <nick.lin@patriotmemory.com>
2025-04-24 17:36:13 +0800

    feat(env): 新增 env type 設定

--- apps/vue-website/env.d.ts
+++ apps/vue-website/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_WEBSITE_TITLE: string
+    readonly VITE_BLOG_NAME: string
+    readonly VITE_BLOG_SLOGAN: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
